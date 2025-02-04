ee7b2bdae25dc4dbf47965d2c765297d61ce6e43
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-05 01:29:16 +0530

    Add Analyze view to remove punctuation from user input

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -6,20 +6,41 @@ def index(request):
     
     return render(request, 'index.html')
 
-def removepunc(request):
-    #Get the text from the user and analyze it
-    djtext=(request.GET.get('text', 'default'))
-    print(djtext)
-    return HttpResponse("<h3>Remove punchuation.</h3>")
+def Analyize(request):
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+    removepunc=request.GET.get('removepunc','off')
 
-def capitalizefirst(request):
-    return HttpResponse("<h3>capitalizefirst.</h3>")
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'Analyize.html', params)
 
-def newlineremove(request):
-    return HttpResponse("<h3>newlineremove.</h3>")
+    else:
+        return HttpResponse('Error')
+    # #Get the text from the user and analyze it
+    # djtext = request.GET.get('text', 'default')
+    # analyzed=djtext
+    # params = {'purpose': "Removing Punctuations", 'analyzed_text': analyzed}
+    # return render(request,'Analyize.html',params)
+    # # djtext=(request.GET.get('text', 'default'))
+    # removepunc=(request.GET.get('removepunc', 'off'))
+    # print(removepunc)
+    # print(djtext)
+    # return HttpResponse("<h3>Remove punchuation.</h3>")
 
-def spaceremove(request):
-    return HttpResponse("<h3>spaceremove.</h3>")
+# def capitalizefirst(request):
+#     return HttpResponse("<h3>capitalizefirst.</h3>")
 
-def charcount(request):
-    return HttpResponse("<h3>charcount.</h3>")
+# def newlineremove(request):
+#     return HttpResponse("<h3>newlineremove.</h3>")
+
+# def spaceremove(request):
+#     return HttpResponse("<h3>spaceremove.</h3>")
+
+# def charcount(request):
+#     return HttpResponse("<h3>charcount.</h3>")
