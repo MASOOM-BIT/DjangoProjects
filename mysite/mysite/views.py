a55282de4dcb66d763968285c7c32ad59dbe6f65
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-06 01:15:57 +0530

    Add functionality for uppercase conversion, newline removal, extra space removal, and character counting in Analyze view

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -10,6 +10,10 @@ def Analyize(request):
     # Get the text
     djtext = request.GET.get('text', 'default')
     removepunc=request.GET.get('removepunc','off')
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
+    charcount=request.GET.get('charcount','off')
 
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -19,7 +23,38 @@ def Analyize(request):
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
         return render(request, 'Analyize.html', params)
+    elif fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'Analyize.html', params)
+    elif newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
 
+        return render(request, 'Analyize.html', params)
+    elif(extraspaceremover=="on"):
+        analyzed = djtext.strip()
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+        # Analyze the text
+        return render(request, 'Analyize.html', params)
+    elif charcount=='on':
+        char_dict = {}
+        for char in djtext:
+            if char in char_dict:
+                char_dict[char] += 1  # Increment count if character exists
+            else:
+                char_dict[char] = 1  # Initialize count if character is new
+        params = {'purpose': 'Character Count', 'analyzed_text': char_dict}
+
+        # Analyze the text
+        return render(request, 'Analyize.html', params)
     else:
         return HttpResponse('Error')
     # #Get the text from the user and analyze it
