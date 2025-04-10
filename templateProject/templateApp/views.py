93208752d4d493756d51280f4d2f7d37f878353b
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-11 01:57:55 +0530

    Add view functions for rendering templates

--- templateProject/templateApp/views.py
+++ templateProject/templateApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def renderTemplate(request):
+    mydict={'name':'Masoom Raza'}
+    return render(request,'templateApp/firstTemplate.html',context=mydict)
+
+def renderEmployee(request):
+    mydict={'name':'Masoom Raza','age':23,'salary':100000}
+    return render(request,'templateApp/employeeTemplate.html',mydict)
