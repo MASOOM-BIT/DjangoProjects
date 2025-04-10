ef5a4ca6ce2856bac0f69973b3dd0bb72bd6d981
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-11 01:58:20 +0530

    Fix typo in templates directory path in settings.py

--- templateProject/templateProject/settings.py
+++ templateProject/templateProject/settings.py
@@ -54,7 +54,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'tempaltes')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
