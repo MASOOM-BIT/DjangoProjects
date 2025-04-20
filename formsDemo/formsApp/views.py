09e1adc243df0482fb490a04445e0ffaec95ace2
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-21 01:43:35 +0530

    Add user registration view logic to formsApp

--- formsDemo/formsApp/views.py
+++ formsDemo/formsApp/views.py
@@ -4,4 +4,13 @@ from formsApp import forms
 
 def userRegistrationView(request):
     form = forms.UserRegistrationForm()
+    if request.method == 'POST':
+        form = forms.UserRegistrationForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            print("User Registration Successful")
+            print("First Name :", form.cleaned_data['firstName'])
+            print("Last Name :", form.cleaned_data['lastName'])
+            print("Email :", form.cleaned_data['email'])
+            
     return render (request, 'formsApp/userRegistration.html', {'form': form})
