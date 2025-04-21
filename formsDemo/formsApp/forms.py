0045152909754b2b1d8e765831b097a53c4f32a4
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-22 01:17:13 +0530

    Refactor UserRegistrationForm to include gender selection and improve validation for first name and email fields

--- formsDemo/formsApp/forms.py
+++ formsDemo/formsApp/forms.py
@@ -1,11 +1,44 @@
 from django import forms
+from django.core import validators
+
 
 class UserRegistrationForm(forms.Form):
-    firstName = forms.CharField(max_length=100, label='First Name')
+    GENDER = [{'male','MALE'},{'female','FEMALE'}]
+    firstName = forms.CharField(max_length=100, label='First Name', validators=[validators.MinLengthValidator(5), validators.MaxLengthValidator(20)])
     lastName = forms.CharField(max_length=100, label='Last Name')
-    email = forms.EmailField(label='Email')
+    email = forms.EmailField(label='Email',required=False)
+    gender=forms.CharField(widget=forms.Select(choices=GENDER))
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
+'''
+def clean_firstName(self):
+    inputFirstName = self.cleaned_data['firstName']
+    if len(inputFirstName) > 20:
+        raise forms.ValidationError("The max length of first name is 20 characters")
+    return inputFirstName
+
+
+def clean_email(self):
+    inputEmail = self.cleaned_data['email']
+    if inputEmail.find('@') == -1:
+        raise forms.ValidationError("Email should contain @")
+    return inputEmail
+
+# -------------single clean function-----
+def clean(self):
+    cleaned_data = super().clean()
+    inputFirstName = cleaned_data.get('firstName')
+    inputEmail = cleaned_data.get('email')
+    
+    if len(inputFirstName) > 20:
+        self.add_error('firstName', "The max length of first name is 20 characters")
+    
+    if inputEmail and inputEmail.find('@') == -1:
+        self.add_error('email', "Email should contain @")
+    
+    return cleaned_data
+    '''
 # CSRF : cross site request forgery {% csrf_token %}
 
 # form.as_p
