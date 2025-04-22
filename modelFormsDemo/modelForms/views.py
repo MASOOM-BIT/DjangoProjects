ce1d2264c356ccc43786f1e9012bdb6eecc9ca8a
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-23 01:17:24 +0530

    Add views for project management including index, listProjects, and addProject

---
+++ modelFormsDemo/modelForms/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from modelForms.forms import ProjectForm
+from modelForms.models import Project
+# Create your views here.
+
+def index(request):
+    return render(request,'modelForms/index.html')
+
+def listProjects(request):
+    projectsList = Project.objects.all()
+    context = {
+        'projects': projectsList
+    }
+    return render(request,'modelForms/listProjects.html',context)
+
+def addProject(request):
+    form=ProjectForm()
+    if form.method=='POST':
+        form=ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    context = {'form':form}
+    return render(request,'modelForms/addProject.html',context)
