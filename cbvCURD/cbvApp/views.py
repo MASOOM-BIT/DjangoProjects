ca79c8c3659184691b873ea898e21d671faa1371
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-02 02:31:29 +0530

    Add detail and create views for Student model in views.py

--- cbvCURD/cbvApp/views.py
+++ cbvCURD/cbvApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView
 from cbvApp.models import Student
 # Create your views here.
 
@@ -10,3 +10,14 @@ class StudentListView(ListView):
     #default template_name is student_list.html
     # template_name = 'student_list.html'
     # default context_object_name = 'student_list'
+
+class StudentDetailView(DetailView):
+    model=Student
+    # default template_name is student_detail.html
+    # template_name = 'student_detail.html'
+    # default context_object_name = 'student'
+
+class StudentCreateView(CreateView):
+    model=Student
+    fields=['firstName','lastName','testScore']
+    # template_name = 'student_form.html'
