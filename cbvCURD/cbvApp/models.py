a869cefd67c80aab85ef083b691150c763c37560
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-03 02:06:20 +0530

    Add get_absolute_url method to Student model for detail view navigation

--- cbvCURD/cbvApp/models.py
+++ cbvCURD/cbvApp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Student(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     testScore = models.FloatField()
+
+    def get_absolute_url(self):
+        return reverse("StudentDetailView", kwargs={"pk": self.pk})
+    
