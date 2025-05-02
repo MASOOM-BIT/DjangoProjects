538c3ee19a35f3ccc2e77456dd9ce6bd3a9e391b
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-03 02:06:34 +0530

    Add update and delete views for Student model

--- cbvCURD/cbvApp/views.py
+++ cbvCURD/cbvApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from cbvApp.models import Student
+from django.urls import reverse_lazy
 # Create your views here.
 
 class StudentListView(ListView):
@@ -21,3 +22,14 @@ class StudentCreateView(CreateView):
     model=Student
     fields=['firstName','lastName','testScore']
     # template_name = 'student_form.html'
+
+class StudentUpdateView(UpdateView):
+    model=Student
+    fields=('testScore',)
+    # template_name = 'student_form.html'
+
+class StudentDeleteView(DeleteView):
+    model=Student
+    # template_name = 'student_confirm_delete.html'
+    # default success_url = '/students/'
+    success_url = reverse_lazy("StudentListView")
