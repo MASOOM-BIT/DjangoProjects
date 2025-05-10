d04a6edbe3191cef9f2471012ed0c6b80815e203
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-11 02:48:15 +0530

    Add views for session management and item handling in sessionApp

---
+++ sessionAPI/sessionApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .forms import ItemForm
+from django.http import HttpResponse
+# Create your views here.
+def pageCount(request):
+    count = request.session.get('count', 0)
+    count=count+1
+    request.session['count'] = count
+    return render(request, 'sessionApp/count.html',{'count': count})
+
+def indexView(request):
+    request.session.set_expiry(120)
+    del request.session['count']
+    return render(request, 'sessionApp/index.html')
+
+def addItem(request):
+    form = ItemForm()
+    if request.method == 'POST':
+        name = request.POST['name']
+        quantity = request.POST['quantity']
+        request.session[name] = quantity
+    return render(request, 'sessionApp/addItem.html', {'form': form})
+    
+
+def displayView(request):
+    return render(request, 'sessionApp/displayView.html', {'cookies': request.COOKIES})
