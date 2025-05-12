995fe1a689212b4144364156d9c61fc1bab9fb43
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-13 02:13:55 +0530

    Add login_required decorator to student management views

--- fbvCURD/fbvApp/views.py
+++ fbvCURD/fbvApp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render,redirect
 from fbvApp.models import Student
 from fbvApp.forms import StudentForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def getStudents(request):
     students = Student.objects.all()
     return render(request, 'fbvApp/index.html', {'students': students})
 
-
+@login_required
 def createStudent(request):
     form = StudentForm()
     if request.method=='POST':
@@ -17,12 +19,12 @@ def createStudent(request):
         return redirect('/')
     return render(request, 'fbvApp/create.html', {'form': form})
 
-
+@login_required
 def deleteStudent(request,id):
     student = Student.objects.get(id=id)
     student.delete()
     return redirect('/')
-
+@login_required
 def updateStudent(request,id):
     student = Student.objects.get(id=id)
     form = StudentForm(instance=student)
