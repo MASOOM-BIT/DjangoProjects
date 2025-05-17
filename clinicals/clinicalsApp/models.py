29210f69402c312cfa4168061cf411f4c073b6be
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-18 02:45:14 +0530

    Add Patient and ClinicalData models to clinicalsApp package

--- clinicals/clinicalsApp/models.py
+++ clinicals/clinicalsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    lastName = models.CharField(max_length=100)
+    firstName = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class ClinicalData(models.Model):
+    COMONENT_NAMES = [('hw', 'Height/Weight'), ('bp', 'Blood Pressure'), ('heartrate', 'Heart Rate')]
+    componentName = models.CharField(choices=COMONENT_NAMES,max_length=100)
+    componentValue = models.CharField(max_length=100)
+    measuredDateTime = models.DateTimeField(auto_now_add=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
