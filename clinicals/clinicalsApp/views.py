b5d3dac982c45c2d888b110346e1166fbcbc081b
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-19 02:04:10 +0530

    Refactor patient views to enhance readability and maintainability

--- clinicals/clinicalsApp/views.py
+++ clinicals/clinicalsApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from clinicalsApp.models import Patient
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy 
 # Create your views here.
+
+class PatientListView(ListView):
+    model = Patient
+    #template_name = 'clinicalsApp/patient_list.html'
+
+class PatientCreateView(CreateView):
+    model = Patient
+    success_url = reverse_lazy('index')
+    fields = ('firstName', 'lastName', 'age')
+    #template_name = 'clinicalsApp/patient_form.html'
+
+class PatientUpdateView(UpdateView):
+    model = Patient
+    success_url = reverse_lazy('index')
+    fields = ('firstName', 'lastName', 'age')
+    #template_name = 'clinicalsApp/patient_form.html'
+
+class PatientDeleteView(DeleteView):
+    model= Patient
+    success_url = reverse_lazy('index')
+    #template_name = 'clinicalsApp/patient_confirm_delete.html'
+
