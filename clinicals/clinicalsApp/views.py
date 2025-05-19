482fd23f28f709c6dfdecb949770224169a2f938
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-20 02:22:05 +0530

    Add addData view for handling clinical data submissions

--- clinicals/clinicalsApp/views.py
+++ clinicals/clinicalsApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from clinicalsApp.models import Patient
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
-from django.urls import reverse_lazy 
+from django.urls import reverse_lazy
+from clinicalsApp.forms import ClinicalDataForm
 # Create your views here.
 
 class PatientListView(ListView):
@@ -24,4 +25,15 @@ class PatientDeleteView(DeleteView):
     model= Patient
     success_url = reverse_lazy('index')
     #template_name = 'clinicalsApp/patient_confirm_delete.html'
+
+def addData(request,**kwargs):
+    form = ClinicalDataForm()
+    patient = Patient.objects.get(id=kwargs['pk'])
+    if request.method == 'POST':
+        form = ClinicalDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request,'clinicalsApp/clinicaldata_form.html',{'form':form,'patient':patient})
+
 
